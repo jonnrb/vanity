07b16edf8f616838538da927b04ee3d89b3b2df9
Jon Betti <jonbetti@gmail.com>
2018-06-04 23:04:11 -0400

    Update example

--- example_test.go
+++ example_test.go
@@ -2,8 +2,16 @@ package vanity
 
 import (
 	"net/http"
+
+	"go.jonnrb.io/vanity"
 )
 
 func ExampleRedirect() {
-	http.Handle("/", Redirect("git", "kkn.fi", "github.com/kare"))
+	// Redirects the vanity import path "go.jonnrb.io/vanity" to the code hosted
+	// on GitHub by user (or organization) "jonnrb" in repo "vanity" using the
+	// git over "https".
+	h := vanity.GitHubHandler("go.jonnrb.io/vanity", "jonnrb", "vanity", "https")
+
+	http.Handle("/vanity", h)
+	http.Handle("/vanity/", h) // to handle requests for subpackages.
 }
