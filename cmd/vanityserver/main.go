8de3d17730a28d0d6d27bdfd9f2f76a3f1b3e461
Jon Betti <jonbetti@gmail.com>
2018-06-06 01:04:35 -0400

    Embellish docstring

--- cmd/vanityserver/main.go
+++ cmd/vanityserver/main.go
@@ -1,5 +1,20 @@
 /*
 Runs a barebones vanity server over HTTP.
+
+Usage
+
+  ./vanityserver [-index] fqdn [repo file]
+
+The "-index" flag enables an index page at "/" that lists all repos hosted on
+this server.
+
+If repo file is not given, "./repos" is used. The file has the following format:
+
+  pkgroot  vcsScheme://vcsHost/user/repo
+  pkgroot2 vcsScheme://vcsHost/user/repo2
+
+vcsHost is either a Gogs server (that's what I use) or github.com. I'm open to
+supporting other VCSs but I'm not sure what that would look like.
 */
 package main // go.jonnrb.io/vanity
 
